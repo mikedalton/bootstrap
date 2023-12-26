b5526c0f6eeff805072800234c19ed5058b370b0
Michael Dalton <mikedalton@Michaels-Air.iwillcrazy.com>
2023-12-26 11:56:58 +0900

    Add Plex and fonts

---
+++ fonts.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf -P ~/Library/Fonts/ -q
+wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf -P ~/Library/Fonts/ -q
+wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf -P ~/Library/Fonts/ -q
+wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf -P ~/Library/Fonts/ -q
